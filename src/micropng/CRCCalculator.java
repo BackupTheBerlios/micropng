ead81c31c20cad7217dfa90d198e4c401be95fd7
neo <neo@tacticalops.(none)>
2009-05-30 00:15:40 +0200

    update

---
+++ src/micropng/CRCCalculator.java
@@ -0,0 +1,64 @@
+package micropng;
+
+
+public class CRCCalculator {
+
+	private int[] crcTable;
+	private int crc;
+	private int length;
+	private MicroPNGQueue input;
+	private MicroPNGQueue output;
+
+	public CRCCalculator(MicroPNGQueue input, int length, int[] type) {
+		crcTable = new int[256];
+		crc = 0xffffffff;
+		makeCrcTable();
+		this.input = input;
+		this.length = length;
+		this.output = new MicroPNGQueue();
+
+		for (int i = 0; i < 4; i++) {
+			crc = crcTable[(crc ^ type[i]) & 0x000000ff] ^ (crc >>> 8);
+		}
+	}
+
+	private void makeCrcTable() {
+		for (int i = 0; i < 256; i++) {
+			int c = i;
+			for (int j = 0; j < 8; j++) {
+				if ((c & 0x00000001) != 0) {
+					c = 0xedb88320 ^ (c >>> 1);
+				} else {
+					c >>>= 1;
+				}
+			}
+			crcTable[i] = c;
+		}
+	}
+
+	public void run() {
+		try {
+			for (int i = length; i != 0; i--) {
+				int currentValue = input.take();
+				crc = crcTable[(crc ^ currentValue) & 0x000000ff] ^ (crc >>> 8);
+				output.put(currentValue);
+			}
+
+			output.flush();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} 
+	}
+
+	public int getResult() {
+		return crc ^ 0xffffffff;
+	}
+
+	public boolean compareTo(int referenceValue) {
+		return getResult() == referenceValue;
+	}
+
+	public MicroPNGQueue getOutputQueue() {
+		return output;
+	}
+}
