69806e590e8daa055ac0dfab5e3ee2bdb1caaed4
neo <neo@tacticalops.(none)>
2009-05-31 13:31:34 +0200

    add interface DataField

--- src/micropng/chunk/DataField.java
+++ src/micropng/chunk/DataField.java
@@ -1,5 +1,12 @@
 package micropng.chunk;
 
+import micropng.MicroPNGQueue;
+
 public interface DataField {
 
+    public int getSize();
+    public MicroPNGQueue getStream(int start, int stop);
+    public MicroPNGQueue getStream();
+    public int[] getArray(int start, int stop);
+    public int[] getArray();
 }
