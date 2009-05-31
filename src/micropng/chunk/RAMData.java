04edfd3f95c016d7ce1d2e36e97797623a75404a
neo <neo@tacticalops.(none)>
2009-05-31 23:33:57 +0200

    Start two implementations of new interface DataField.

---
+++ src/micropng/chunk/RAMData.java
@@ -0,0 +1,36 @@
+package micropng.chunk;
+
+import micropng.MicroPNGQueue;
+
+public class RAMData implements DataField {
+
+    private int size;
+
+    @Override
+    public int[] getArray(int start, int stop) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public int[] getArray() {
+	return getArray(0, size);
+    }
+
+    @Override
+    public int getSize() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public MicroPNGQueue getStream(int start, int stop) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public MicroPNGQueue getStream() {
+	return getStream(0, size);
+    }
+}
