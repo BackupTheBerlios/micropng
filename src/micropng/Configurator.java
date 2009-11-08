4dfbac2938c95833630e75fdb035ef1edaf01ed5
neo <neo@tacticalops.(none)>
2009-11-08 01:42:52 +0100

    first bits of Configurator that should have come with the last commit

--- src/micropng/Configurator.java
+++ src/micropng/Configurator.java
@@ -1,10 +1,37 @@
 package micropng;
 
+import java.util.TreeSet;
+
 public class Configurator {
 
+    private TreeSet<Integer> knownChunks;
+
     public Configurator() {
-	
+	knownChunks = new TreeSet<Integer>();
     }
 
-    
+    private int convertStringToInt(String s) {
+	int integerRepresentation = 0;
+	for (int i = 0; i < 4; i++) {
+	    integerRepresentation <<= 8;
+	    integerRepresentation += s.charAt(i);
+	}
+	return integerRepresentation;
+    }
+
+    public void addKnownChunkType(String chunkType) {
+	addKnownChunkType(convertStringToInt(chunkType));
+    }
+
+    public void addKnownChunkType(int chunkType) {
+	knownChunks.add(chunkType);
+    }
+
+    public boolean isKnownChunkType(String chunkType) {
+	return isKnownChunkType(convertStringToInt(chunkType));
+    }
+
+    public boolean isKnownChunkType(int chunkType) {
+	return knownChunks.contains(chunkType);
+    }
 }
