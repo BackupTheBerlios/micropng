05115d691a6c6da64ef3749421e286a43638eea4
neo <neo@tacticalops.(none)>
2009-11-10 01:44:22 +0100

    more getter/setter in Configuration, start over ChunkFactory stub and remove its Exception

--- src/micropng/chunk/ChunkFactory.java
+++
@@ -1,6 +0,0 @@
-package micropng.chunk;
-
-public interface ChunkFactory<T extends Chunk> {
-
-    public T produce(int[] type, FileData data, int crc) throws UnknownCriticalChunkException;
-}
--- src/micropng/chunk/UnknownCriticalChunkException.java
+++
@@ -1,5 +0,0 @@
-package micropng.chunk;
-
-public class UnknownCriticalChunkException extends Throwable {
-
-}
