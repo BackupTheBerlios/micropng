7bef7ce47ada50ea74e368ab4414db41ea9b6935
neo <neo@tacticalops.(none)>
2010-04-11 00:09:46 +0200

    implement SameTypeComparator

--- src/micropng/chunk/SameTypeComparator.java
+++ src/micropng/chunk/SameTypeComparator.java
@@ -1,18 +1,31 @@
 package micropng.chunk;
 
 public enum SameTypeComparator {
-    ALPHABETICAL_ORDERING () {
+    ALPHABETICAL_ORDERING() {
 	@Override
 	public int compare(Chunk chunk0, Chunk chunk1) {
-	    //TODO: implement
-	    return -2;
+	    int length0 = chunk0.getDataSize();
+	    int length1 = chunk1.getDataSize();
+	    int minLength = Math.min(length0, length1);
+	    int pos = 0;
+	    int byte0;
+	    int byte1;
+
+	    while (pos < minLength) {
+		byte0 = chunk0.getByteAt(pos);
+		byte1 = chunk1.getByteAt(pos);
+		if (byte0 != byte1) {
+		    return (byte0 < byte1) ? -1 : 1;
+		}
+		pos++;
+	    }
+	    return (length0 < length1) ? -1 : 1;
 	}
     },
-    KEEP_INPUT_ORDERING () {
+    KEEP_INPUT_ORDERING() {
 	@Override
 	public int compare(Chunk chunk0, Chunk chunk1) {
-	    //TODO: implement
-	    return -2;
+	    return (chunk0.getId() < chunk1.getId()) ? -1 : 1;
 	}
     };
     public abstract int compare(Chunk chunk0, Chunk chunk1);
