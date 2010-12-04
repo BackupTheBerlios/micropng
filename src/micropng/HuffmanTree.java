6222ad82f519db4f14422d9b876633c99e550b2c
neo <neo@tacticalops.(none)>
2010-12-04 23:52:12 +0100

    minor progress in DeflateDecoder

---
+++ src/micropng/HuffmanTree.java
@@ -0,0 +1,110 @@
+package micropng;
+
+public class HuffmanTree {
+    private class HuffmanNode {
+
+	private int value;
+	private HuffmanNode left;
+	private HuffmanNode right;
+
+	public HuffmanNode() {
+
+	}
+
+	public int getValue() {
+	    return value;
+	}
+
+	public void setValue(int value) {
+	    this.value = value;
+	}
+
+	public HuffmanNode getLeft() {
+	    return left;
+	}
+
+	public void setLeft(HuffmanNode left) {
+	    this.left = left;
+	}
+
+	public HuffmanNode getRight() {
+	    return right;
+	}
+
+	public void setRight(HuffmanNode right) {
+	    this.right = right;
+	}
+
+    }
+
+    private class HuffmanTreeWalker {
+	HuffmanNode pos;
+	private HuffmanTreeWalker () {
+	    reset();
+	}
+	public void reset() {
+	    pos = root;
+	}
+
+	public boolean isLeaf() {
+	    return (pos.left == null) && (pos.right == null);
+	}
+    }
+
+    private static final int MAX_BITS = 15;
+
+    private HuffmanNode root;
+
+    public HuffmanTree(int[] codeLengths) {
+	root = new HuffmanNode();
+	int[] bl_count = new int[MAX_BITS + 1];
+	int[] next_code = new int[MAX_BITS + 1];
+	int code = 0;
+
+	for (int i : codeLengths) {
+	    bl_count[i]++;
+	}
+
+	bl_count[0] = 0;
+
+	for (int bits = 1; bits <= MAX_BITS; bits++) {
+	    code = (code + bl_count[bits - 1]) << 1;
+	    next_code[bits] = code;
+	}
+
+	for (int n = 0; n <= codeLengths.length; n++) {
+	    int len = codeLengths[n];
+	    if (len != 0) {
+		addCode(n, next_code[len], len);
+		next_code[len]++;
+	    }
+	}
+    }
+
+    private void addCode(int value, int code, int length) {
+	HuffmanNode currentNode = root;
+	int mask = 0x01 << length;
+	for (int i = 0; i < length; i++) {
+	    HuffmanNode nextNode;
+	    boolean leftTree;
+	    mask >>= 1;
+	    leftTree = ((mask & code) == 0);
+
+	    if (leftTree) {
+		nextNode = currentNode.getLeft();
+		if (nextNode == null) {
+		    nextNode = new HuffmanNode();
+		    currentNode.setLeft(nextNode);
+		}
+	    } else {
+		nextNode = currentNode.getRight();
+		if (nextNode == null) {
+		    nextNode = new HuffmanNode();
+		    currentNode.setRight(nextNode);
+		}
+	    }
+	    currentNode = nextNode;
+	}
+	currentNode.setValue(value);
+    }
+}
