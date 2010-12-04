404e2faa4d8d4378b5c1301a3c58f588771d4c18
neo <neo@tacticalops.(none)>
2010-12-04 23:54:04 +0100

    previous commit was incomplete

--- src/micropng/DeflateDecoder.java
+++ src/micropng/DeflateDecoder.java
@@ -2,102 +2,6 @@ package micropng;
 
 public class DeflateDecoder {
     private class DeflateDecoderThread implements Runnable {
-	private class HuffmanTree {
-
-	    private class HuffmanNode {
-
-		private int value;
-		private HuffmanNode left;
-		private HuffmanNode right;
-
-		public HuffmanNode() {
-
-		}
-
-		public int getValue() {
-		    return value;
-		}
-
-		public void setValue(int value) {
-		    this.value = value;
-		}
-
-		public HuffmanNode getLeft() {
-		    return left;
-		}
-
-		public void setLeft(HuffmanNode left) {
-		    this.left = left;
-		}
-
-		public HuffmanNode getRight() {
-		    return right;
-		}
-
-		public void setRight(HuffmanNode right) {
-		    this.right = right;
-		}
-
-	    }
-
-	    private static final int MAX_BITS = 15;
-
-	    private HuffmanNode root;
-
-	    public HuffmanTree(int[] codeLengths) {
-		root = new HuffmanNode();
-		int[] bl_count = new int[MAX_BITS + 1];
-		int[] next_code = new int[MAX_BITS + 1];
-		int code = 0;
-
-		for (int i : codeLengths) {
-		    bl_count[i]++;
-		}
-
-		bl_count[0] = 0;
-
-		for (int bits = 1; bits <= MAX_BITS; bits++) {
-		    code = (code + bl_count[bits - 1]) << 1;
-		    next_code[bits] = code;
-		}
-
-		for (int n = 0; n <= codeLengths.length; n++) {
-		    int len = codeLengths[n];
-		    if (len != 0) {
-			addCode(n, next_code[len], len);
-			next_code[len]++;
-		    }
-		}
-	    }
-
-	    private void addCode(int value, int code, int length) {
-		HuffmanNode currentNode = root;
-		int mask = 0x01 << length;
-		for (int i = 0; i < length; i++) {
-		    HuffmanNode nextNode;
-		    boolean leftTree;
-		    mask >>= 1;
-		    leftTree = ((mask & code) == 0);
-
-		    if (leftTree) {
-			nextNode = currentNode.getLeft();
-			if (nextNode == null) {
-			    nextNode = new HuffmanNode();
-			    currentNode.setLeft(nextNode);
-			}
-		    } else {
-			nextNode = currentNode.getRight();
-			if (nextNode == null) {
-			    nextNode = new HuffmanNode();
-			    currentNode.setRight(nextNode);
-			}
-		    }
-		    currentNode = nextNode;
-		}
-		currentNode.setValue(value);
-	    }
-	}
-
 	private Queue input;
 	private Queue output;
 	private int pos;
@@ -108,14 +12,6 @@ public class DeflateDecoder {
 	    this.output = output;
 	    this.pos = 0;
 	    this.currentByte = input.take();
-	}
-
-	private int readValue() {
-	    return 0;
-	}
-
-	private void readHuffmanTrees() {
-	    
 	}
 
 	/*
@@ -188,31 +84,28 @@ public class DeflateDecoder {
 	    int numberOfLiteralAndLengthCodes = HLIT + 257;
 	    int numberOfDistanceCodes = HDIST + 1;
 	    int numberOfCodeLengthCodes = HCLEN + 4;
-	    int[] codeLengthCodes = new int[19];
+	    int[] codeLengthCodes = new int[numberOfCodeLengthCodes];
 	    int[] codeLengthCodesOrder = { 16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2,
 		    14, 1, 15 };
-	    int numberOfLiteralAndLengthCodesRead = 0;
-	    int numberOfDistanceCodesRead = 0;
+	    HuffmanTree codeLengthsTree;
+	    int[] literalLengthAndDistanceCodesTable = new int[numberOfLiteralAndLengthCodes + numberOfDistanceCodes];
+	    int posInTable = 0;
 
 	    for (int i = 0; i < numberOfCodeLengthCodes; i++) {
 		codeLengthCodes[codeLengthCodesOrder[i]] = getNextBits(3);
 	    }
 
-	    /*while (numberOfLiteralAndLengthCodesRead < numberOfLiteralAndLengthCodes) {
-		numberOfLiteralAndLengthCodesRead++; //TODO
+	    codeLengthsTree = new HuffmanTree(codeLengthCodes);
+
+	    while (posInTable < literalLengthAndDistanceCodesTable.length) {
+		
 	    }
 
-	    while (numberOfDistanceCodesRead < numberOfDistanceCodes) {
-		numberOfDistanceCodesRead++; //TODO
-	    }*/
-	    //argh: The code length repeat codes can cross from HLIT + 257 to the
-	    // HDIST + 1 code lengths.
-    
 	    // read codes while not value == 256
 	    
 	}
 
-	private boolean readBlock() throws InterruptedException {
+	private boolean readDeflateBlock() throws InterruptedException {
 	    boolean res;
 	    int BFINAL;
 	    int BTYPE = 0;
@@ -243,7 +136,7 @@ public class DeflateDecoder {
 	    try {
 		boolean lastBlockRead = false;
 		while (!lastBlockRead) {
-		    lastBlockRead = readBlock();
+		    lastBlockRead = readDeflateBlock();
 		}
 		output.close();
 	    } catch (InterruptedException e) {
