f25b321237af32206e7db8807150128dd5217da3
Martin Walch <neo@tacticalops.(none)>
2011-01-23 17:14:28 +0100

    everything in Configurator has been superseeded, class reduced to a stub

--- src/micropng/micropng/Configurator.java
+++ src/micropng/micropng/Configurator.java
@@ -1,37 +1,7 @@
 package micropng.micropng;
 
-import java.util.TreeSet;
+import micropng.chunkview.chunk.Type;
 
 public class Configurator {
 
-    private TreeSet<Integer> knownChunks;
-
-    public Configurator(Configuration userConfiguration) {
-	knownChunks = new TreeSet<Integer>();
-    }
-
-    private int convertStringToInt(String s) {
-	int integerRepresentation = 0;
-	for (int i = 0; i < 4; i++) {
-	    integerRepresentation <<= 8;
-	    integerRepresentation += s.charAt(i);
-	}
-	return integerRepresentation;
-    }
-
-    public void addKnownChunkType(String chunkType) {
-	addKnownChunkType(convertStringToInt(chunkType));
-    }
-
-    public void addKnownChunkType(int chunkType) {
-	knownChunks.add(chunkType);
-    }
-
-    public boolean isKnownChunkType(String chunkType) {
-	return isKnownChunkType(convertStringToInt(chunkType));
-    }
-
-    public boolean isKnownChunkType(int chunkType) {
-	return knownChunks.contains(chunkType);
-    }
 }
