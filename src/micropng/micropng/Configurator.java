3f6e95b38ff5559e913d9a2d883f3cb0a1ae19e6
Martin Walch <neo@tacticalops.(none)>
2011-01-23 17:41:32 +0100

    new method stub: Configurator.sanitize()

--- src/micropng/micropng/Configurator.java
+++ src/micropng/micropng/Configurator.java
@@ -1,7 +1,13 @@
 package micropng.micropng;
 
 import micropng.chunkview.chunk.Type;
+import micropng.micropng.Configuration.Preset;
 
 public class Configurator {
 
+    public Configuration sanitize(Configuration userConfiguration) {
+	Configuration res = Configuration.createNewConfig(Preset.NOOP);
+	res.setFilename(userConfiguration.getFilename());
+	return res;
+    }
 }
