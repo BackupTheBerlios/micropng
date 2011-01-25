f0598e7c0bf77adcf61cab28c346569da2214d7b
Martin Walch <neo@tacticalops.(none)>
2011-01-25 01:00:50 +0100

    refactored io and configuration, do it without locks

--- src/micropng/micropng/ConfigurationListener.java
+++ src/micropng/micropng/ConfigurationListener.java
@@ -2,6 +2,6 @@ package micropng.micropng;
 
 import micropng.userinterface.OutputChannel;
 
-public interface ConfiguratorListener {
+public interface ConfigurationListener {
     public void configurationMessage(OutputChannel channel, String message);
 }
