c910b566743b7ba14f0b4a50b1ebb036ef216535
Martin Walch <neo@tacticalops.(none)>
2011-11-04 01:56:06 +0100

    new class Dimensions to encapsulate width and length

---
+++ src/micropng/micropng/Dimensions.java
@@ -0,0 +1,21 @@
+package micropng.micropng;
+
+public class Dimensions {
+    private long width;
+    private long height;
+
+    public Dimensions(long width, long height) {
+	super();
+	this.width = width;
+	this.height = height;
+    }
+
+    public long getWidth() {
+	return width;
+    }
+
+    public long getHeight() {
+	return height;
+    }
+
+}
