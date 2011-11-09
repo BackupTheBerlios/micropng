e057556cdf7d1c0d0a18fb528bde383730e03e44
Martin Walch <neo@tacticalops.(none)>
2011-11-09 03:20:14 +0100

    stubs for some new classes

---
+++ src/micropng/userinterface/inputoptions/InvocationHelp.java
@@ -0,0 +1,45 @@
+package micropng.userinterface.inputoptions;
+
+import java.util.ArrayList;
+
+import micropng.commonlib.Status;
+
+public class InvocationHelp implements Parameter {
+
+    @Override
+    public String getLongHelp() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public String getLongParameterName() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public String getShortHelp() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public char getShortParameterName() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public boolean takesArgument() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    @Override
+    public Status validateAndSet(ArrayList<String> values) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
