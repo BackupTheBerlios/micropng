bfdb079934426b2ae67827652f253fa69bf0266b
Martin Walch <neo@tacticalops.(none)>
2011-11-22 01:18:01 +0100

    missing stub GroupNode

---
+++ src/micropng/userinterface/inputoptions/GroupNode.java
@@ -0,0 +1,12 @@
+package micropng.userinterface.inputoptions;
+
+import java.util.ArrayList;
+
+public class GroupNode implements ParameterTreeNode {
+
+    private ArrayList<ParameterTreeNode> childNodes;
+
+    public GroupNode() {
+
+    }
+}
