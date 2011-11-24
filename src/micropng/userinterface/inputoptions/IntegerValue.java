5efeac3664257a08e988fc42e9b1f7b89169f830
Martin Walch <neo@tacticalops.(none)>
2011-11-24 17:46:00 +0100

    add new ParameterValue IntegerValue

---
+++ src/micropng/userinterface/inputoptions/IntegerValue.java
@@ -0,0 +1,38 @@
+package micropng.userinterface.inputoptions;
+
+import micropng.commonlib.Status;
+
+public class IntegerValue implements ParameterValue<Long> {
+
+    private long value;
+    private long lowerBound;
+    private long upperBound;
+
+    public IntegerValue() {
+	this.lowerBound = Long.MIN_VALUE;
+	this.upperBound = Long.MAX_VALUE;
+    }
+
+    public IntegerValue(long lowerBound, long upperBound) {
+	this.lowerBound = lowerBound;
+	this.upperBound = upperBound;
+    }
+
+    @Override
+    public Long getValue() {
+	return value;
+    }
+
+    @Override
+    public Status trySetting(Long value) {
+	if (value < lowerBound) {
+	    return Status.error("Wert zu klein");
+	}
+	if (value > upperBound) {
+	    return Status.error("Wert zu groß");
+	}
+
+	this.value = value;
+	return Status.ok();
+    }
+}
