fb52ce420ed6eb025c6ad18d78c046efd6a5f0ac
Martin Walch <neo@tacticalops.(none)>
2011-11-25 00:20:37 +0100

    adapt ParameterTree and ParameterGroup to use new description structure

--- src/micropng/userinterface/inputoptions/ParameterGroup.java
+++ src/micropng/userinterface/inputoptions/ParameterGroup.java
@@ -4,10 +4,34 @@ import java.util.ArrayList;
 
 public class ParameterGroup implements ParameterTreeNode {
     private ParameterGroupDescription description;
-    private ArrayList<ParameterTreeNode> childNodes;
+    private ArrayList<Parameter> parameters;
+    private ArrayList<ParameterGroup> subGroups;
 
-    public ParameterGroup(ParameterGroupDescription description, ArrayList<ParameterTreeNode> childNodes) {
+    public ParameterGroup(ParameterGroupDescription description, ArrayList<Parameter> parameters,
+	    ArrayList<ParameterGroup> subGroups) {
 	this.description = description;
-	this.childNodes = childNodes;
+	this.parameters = parameters;
+	this.subGroups = subGroups;
+    }
+
+    public ParameterGroupDescription getDescription() {
+	return description;
+    }
+
+    public ArrayList<Parameter> getParameters() {
+	return parameters;
+    }
+
+    public ArrayList<ParameterGroup> getSubGroups() {
+	return subGroups;
+    }
+
+    public ArrayList<Parameter> getAllDependingParameters() {
+	ArrayList<Parameter> res = new ArrayList<Parameter>();
+	res.addAll(parameters);
+	for (ParameterGroup g : subGroups) {
+	    res.addAll(g.getAllDependingParameters());
+	}
+	return res;
     }
 }
--- src/micropng/userinterface/inputoptions/ParameterTree.java
+++ src/micropng/userinterface/inputoptions/ParameterTree.java
@@ -2,10 +2,14 @@ package micropng.userinterface.inputoptions;
 
 public class ParameterTree {
     ParameterTreeDescription description;
-    private ParameterTreeNode root;
+    private ParameterGroup root;
 
-    public ParameterTree(ParameterTreeDescription description, ParameterTreeNode root) {
+    public ParameterTree(ParameterTreeDescription description, ParameterGroup root) {
 	this.description = description;
 	this.root = root;
+    }
+
+    public ParameterGroup getRootNode() {
+	return root;
     }
 }
