f8b45e63a5ecda0a312a5a62476e9b520532e560
Martin Walch <neo@tacticalops.(none)>
2011-12-18 17:58:57 +0100

    add OutputFilename to parameters

---
+++ src/micropng/userinterface/inputoptions/OutputFilename.java
@@ -0,0 +1,37 @@
+package micropng.userinterface.inputoptions;
+
+public class OutputFilename implements ParameterDescription {
+    private static final String longHelp = "Der Pfad zu einer Ausgabedatei. Jede Art von Pfad, der von der JVM verarbeitet werden kann, ist erlaubt. Die Datei darf noch nicht existieren.";
+    private static final String longParameterName = "output-file";
+    private static final String shortHelp = "Pfad der Ausgabedatei";
+    private static final char shortParameterName = 'o';
+    private static final Path defaultValue = new Path();
+    static {
+	defaultValue.trySetting(null);
+    }
+
+    @Override
+    public ParameterValue<?> defaultValue() {
+	return defaultValue.clone();
+    }
+
+    @Override
+    public String getLongHelp() {
+	return longHelp;
+    }
+
+    @Override
+    public String getLongParameterName() {
+	return longParameterName;
+    }
+
+    @Override
+    public String getShortHelp() {
+	return shortHelp;
+    }
+
+    @Override
+    public char getShortParameterName() {
+	return shortParameterName;
+    }
+}
