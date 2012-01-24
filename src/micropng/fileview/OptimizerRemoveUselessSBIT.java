d45e06261aff42f94a256a4fe0b4be4d4a3199ac
Martin Walch <neo@tacticalops.(none)>
2012-01-24 02:27:26 +0100

    update Optimizer, add new stub for OptimizerRemoveUselessSBIT

---
+++ src/micropng/fileview/OptimizerRemoveUselessSBIT.java
@@ -0,0 +1,5 @@
+package micropng.fileview;
+
+public class OptimizerRemoveUselessSBIT {
+
+}
--- src/micropng/micropng/Optimizer.java
+++ src/micropng/micropng/Optimizer.java
@@ -7,34 +7,29 @@ import micropng.chunkview.chunk.OrganisationSequence;
 import micropng.fileview.OptimizerChunkAggregation;
 import micropng.fileview.OptimizerOrdering;
 import micropng.pngio.FileWriter;
-import micropng.userinterface.UserConfiguration;
+import micropng.userinterface.InternalConfiguration;
 
 public class Optimizer {
-
-    private UserConfiguration configuration;
-    private OptimizerOrdering ordering;
-    private OptimizerChunkAggregation aggregation;
+    private InternalConfiguration configuration;
     private File outputFileObject;
-    private boolean configurationIsSane;
 
-    public Optimizer(UserConfiguration userConfiguration) throws IOException {
-	this.configuration = userConfiguration;
-
-	this.ordering = new OptimizerOrdering();
-	this.aggregation = new OptimizerChunkAggregation();
+    public Optimizer(InternalConfiguration configuration) throws IOException {
+	this.configuration = configuration;
     }
 
     public void run() throws IOException {
-	if (!configurationIsSane) {
-	    return;
-	}
-	//OrganisationSequence chunkOrganisationSequence = new OrganisationSequence(configuration.getChunkSequence());
+	OrganisationSequence chunkOrganisationSequence = new OrganisationSequence(configuration.getChunkSequence());
+	OptimizerOrdering ordering;
+	OptimizerChunkAggregation aggregation;
 
-	//ordering.optimize(chunkOrganisationSequence);
-	//aggregation.optimize(chunkOrganisationSequence);
+	ordering = new OptimizerOrdering();
+	aggregation = new OptimizerChunkAggregation();
+	
+	ordering.optimize(chunkOrganisationSequence);
+	aggregation.optimize(chunkOrganisationSequence);
 
 	//outputFileObject = new File(configuration.getPath() + "_output.png");
 	FileWriter writer = new FileWriter();
-	//writer.writeSequence(outputFileObject, chunkOrganisationSequence.toChunkSequence());
+	writer.writeSequence(outputFileObject, chunkOrganisationSequence.toChunkSequence());
     }
 }
