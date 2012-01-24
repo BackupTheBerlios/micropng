f3a39451dbce6eb33c5627708cd8fb2797010cad
Martin Walch <neo@tacticalops.(none)>
2012-01-24 17:00:26 +0100

    update InternalConfiguration for upcoming release

--- src/micropng/userinterface/InternalConfiguration.java
+++ src/micropng/userinterface/InternalConfiguration.java
@@ -3,103 +3,104 @@ package micropng.userinterface;
 import micropng.chunkview.ChunkSequence;
 
 public class InternalConfiguration {
-    private boolean informationalMode;
-    private boolean optimizeHuffmanTrees;
-    private boolean regroupIDATChunks;
-    private boolean removeAncillaryChunks;
-    private boolean sortChunks;
-    private boolean reencodeZlibStreams;
-    private boolean unknownMandatoryChunkInResult;
-    private boolean unknownAncillaryChunkInResult;
-    private int verbosity;
-    private String[] ancillaryChunksToKeep;
+    // private String[] ancillaryChunksToKeep;
     private ChunkSequence chunkSequence;
+    // private boolean informationalMode;
+    // private boolean optimizeHuffmanTrees;
+    // private boolean reencodeZlibStreams;
+    private boolean regroupIDATChunks;
+    // private boolean removeAncillaryChunks;
+    private boolean sortChunks;
+    private boolean unknownMandatoryChunk;
+    private boolean unknownAncillaryChunk;
 
-    public boolean unknownMandatoryChunkInResult() {
-	return unknownMandatoryChunkInResult;
-    }
+    // private int verbosity;
 
-    void setUnknownMandatoryChunkInResult(boolean unknownMandatoryChunkInResult) {
-	this.unknownMandatoryChunkInResult = unknownMandatoryChunkInResult;
-    }
-
-    void setUnknownAncillaryChunkInResult(boolean unknownAncillaryChunkInResult) {
-	this.unknownAncillaryChunkInResult = unknownAncillaryChunkInResult;
-    }
-
-    public boolean unknownAncillaryChunkInResult() {
-	return unknownAncillaryChunkInResult;
-    }
-
-    public boolean informationalMode() {
-	return informationalMode;
-    }
-
-    public boolean optimizeHuffmanTrees() {
-	return optimizeHuffmanTrees;
-    }
-
-    public boolean regroupIDATChunks() {
-	return regroupIDATChunks;
-    }
-
-    public boolean removeAncillaryChunks() {
-	return removeAncillaryChunks;
-    }
-
-    public boolean sortChunks() {
-	return sortChunks;
-    }
-
-    public boolean reencodeZlibStreams() {
-	return reencodeZlibStreams;
-    }
-
-    public int getVerbosity() {
-	return verbosity;
-    }
-
-    void setInformationalMode(boolean informationalMode) {
-	this.informationalMode = informationalMode;
-    }
-
-    void setOptimizeHuffmanTrees(boolean optimizeHuffmanTrees) {
-	this.optimizeHuffmanTrees = optimizeHuffmanTrees;
-    }
-
-    void setRegroupIDATChunks(boolean regroupIDATChunks) {
-	this.regroupIDATChunks = regroupIDATChunks;
-    }
-
-    void setRemoveAncillaryChunks(boolean removeAncillaryChunks) {
-	this.removeAncillaryChunks = removeAncillaryChunks;
-    }
-
-    void setSortChunks(boolean sortChunks) {
-	this.sortChunks = sortChunks;
-    }
-
-    void setReencodeZlibStreams(boolean reencodeZlibStreams) {
-	this.reencodeZlibStreams = reencodeZlibStreams;
-    }
-
-    void setVerbosity(int verbosity) {
-	this.verbosity = verbosity;
-    }
-
-    void setAncillaryChunksToKeep(String[] ancillaryChunksToKeep) {
-	this.ancillaryChunksToKeep = ancillaryChunksToKeep;
-    }
-
-    public String[] getAncillaryChunksToKeep() {
-	return ancillaryChunksToKeep;
+    public ChunkSequence getChunkSequence() {
+	return chunkSequence;
     }
 
     void setChunkSequence(ChunkSequence chunkSequence) {
 	this.chunkSequence = chunkSequence;
     }
 
-    public ChunkSequence getChunkSequence() {
-	return chunkSequence;
+    public boolean regroupIDATChunks() {
+	return regroupIDATChunks;
     }
+
+    void setRegroupIDATChunks(boolean regroupIDATChunks) {
+	this.regroupIDATChunks = regroupIDATChunks;
+    }
+
+    public boolean sortChunks() {
+	return sortChunks;
+    }
+
+    void setSortChunks(boolean sortChunks) {
+	this.sortChunks = sortChunks;
+    }
+
+    public boolean unknownAncillaryChunk() {
+	return unknownAncillaryChunk;
+    }
+
+    void setUnknownAncillaryChunk(boolean unknownAncillaryChunk) {
+	this.unknownAncillaryChunk = unknownAncillaryChunk;
+    }
+
+    public boolean unknownMandatoryChunk() {
+	return unknownMandatoryChunk;
+    }
+
+    void setUnknownMandatoryChunk(boolean unknownMandatoryChunk) {
+	this.unknownMandatoryChunk = unknownMandatoryChunk;
+    }
+
+    // public boolean informationalMode() {
+    // return informationalMode;
+    // }
+    //
+    // public boolean optimizeHuffmanTrees() {
+    // return optimizeHuffmanTrees;
+    // }
+    //
+    // public boolean removeAncillaryChunks() {
+    // return removeAncillaryChunks;
+    // }
+    //
+    // public boolean reencodeZlibStreams() {
+    // return reencodeZlibStreams;
+    // }
+    //
+    // public int getVerbosity() {
+    // return verbosity;
+    // }
+    //
+    // void setInformationalMode(boolean informationalMode) {
+    // this.informationalMode = informationalMode;
+    // }
+    //
+    // void setOptimizeHuffmanTrees(boolean optimizeHuffmanTrees) {
+    // this.optimizeHuffmanTrees = optimizeHuffmanTrees;
+    // }
+
+    // void setRemoveAncillaryChunks(boolean removeAncillaryChunks) {
+    // this.removeAncillaryChunks = removeAncillaryChunks;
+    // }
+
+    // void setReencodeZlibStreams(boolean reencodeZlibStreams) {
+    // this.reencodeZlibStreams = reencodeZlibStreams;
+    // }
+    //
+    // void setVerbosity(int verbosity) {
+    // this.verbosity = verbosity;
+    // }
+    //
+    // void setAncillaryChunksToKeep(String[] ancillaryChunksToKeep) {
+    // this.ancillaryChunksToKeep = ancillaryChunksToKeep;
+    // }
+    //
+    // public String[] getAncillaryChunksToKeep() {
+    // return ancillaryChunksToKeep;
+    // }
 }
